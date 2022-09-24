86a77ac5980cebcf4e0a423dcd20ecca49fac939
TuanHung <Hungwap375@gmail.com>
2022-09-24 20:03:28 +0700

    fix

--- header_dev.go
+++ header_dev.go
@@ -4,7 +4,7 @@ package main
 // @version 1.0
 // @description API lists for E-Rescues
 
-// @host emotional.up.railway.app
+// @host rescuse.up.railway.app
 // @BasePath /api/v1
 
 // @securityDefinitions.apikey ApiKeyAuth
