fa161bb98fb716ee0e56a32d20b7f2ef250dbb4e
ManhKim <manhtokim@gmail.com>
2022-09-24 20:10:46 +0700

    update swagger

--- docs/docs.go
+++ docs/docs.go
@@ -1362,7 +1362,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:19000",
+	Host:             "rescuse.up.railway.app",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "Swagger UI for E-Rescues",
